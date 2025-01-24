a68fe269eff8616401cce405be075535bc2eacfa
Dipak-dud <dipakdudhagundi@gmail.com>
2025-01-24 16:14:22 +0530

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+package $org.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public static void main( String[] args )
+{
+    System.out.println( "Hello World!" );
+}
