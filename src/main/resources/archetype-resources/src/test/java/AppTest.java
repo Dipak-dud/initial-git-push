0463890ff841345bb04f424d8eab1cb418276aaa
Dipak-dud <dipakdudhagundi@gmail.com>
2025-01-24 16:22:31 +0530

    Code changes

--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -4,7 +4,10 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.net.StandardSocketOptions;
+
 public static void main( String[] args )
 {
     System.out.println( "Hello World!" );
+    System.out,println( "Git push done" ); //added line
 }
